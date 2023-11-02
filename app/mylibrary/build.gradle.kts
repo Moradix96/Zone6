fb258d2176be5c3723c4ca55e9fefb762622900a
Sina Moradi <Moradix96@gmail.com>
2023-11-02 19:39:18 +0330

    Added publishing to modeule build.gradle.kts

--- app/mylibrary/build.gradle.kts
+++ app/mylibrary/build.gradle.kts
@@ -42,3 +42,17 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
+
+publishing {
+    publications {
+        register<MavenPublication>("release") {
+            groupId = "ir.samiantec"
+            artifactId = "my-library"
+            version = "1.0"
+
+            afterEvaluate {
+                from(components["release"])
+            }
+        }
+    }
+}
